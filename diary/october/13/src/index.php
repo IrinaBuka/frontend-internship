3b931f58dc5114645ca6b70ea64aa4534a1c3e30
Andrey Miskov <amiskov@gmail.com>
2016-10-14 11:03:23 +0300

    Fix php server description

--- diary/october/13/src/index.php
+++ diary/october/13/src/index.php
@@ -3,9 +3,12 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Document</title>
-	<link rel="stylesheet" href="css/all.css">
+	<link rel="stylesheet" href="dist/css/all.css">
+    <link rel="stylesheet" href="node_modules/slick-carousel/slick/slick.css">
 </head>
 <body>
 	<?php echo 'Hello form PHP';?>
+    <script src="node_modules/jquery/dist/jquery.js"></script>
+    <script src="node_modules/slick-carousel/slick/slick.js"></script>
 </body>
 </html>
